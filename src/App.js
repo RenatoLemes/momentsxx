30ae7cc87ec743507ffe78c7106b44fd0f929772
RenatoLemes <renato.lemmes@hotmail.com>
2023-07-19 14:28:44 +0000

    Added sign permition for 24hrs

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 
+
 function App() {
+
   return (
+
     <div className={styles.App}>
       <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path = "/" render={() => <h1>Home Page</h1>}/>
-          <Route exact path = "/signin" render={() => <SignInForm />}/>
-          <Route exact path = "/signup" render={() => <SignUpForm />}/>
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path = "/" render={() => <h1>Home Page</h1>}/>
+            <Route exact path = "/signin" render={() => <SignInForm />}/>
+            <Route exact path = "/signup" render={() => <SignUpForm />}/>
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
+      </div>
   );
 }
 
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drf-api-renato-5c98f714c67a.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
